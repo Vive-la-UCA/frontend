bae09694c09ea09968edc06ed711c6e6980e4685
Josse2002 <00048322@uca.edu.sv>
2024-03-20 06:37:50 -0600

    :heavy_plus_sign: Add Leaflet map dependency

--- src/app/dashboard/locations/create-location/page.jsx
+++ src/app/dashboard/locations/create-location/page.jsx
@@ -8,7 +8,6 @@ export default function Page() {
         <div>
             <h1 className="text-3xl font-semibold mb-4">Crear una localidad</h1>
             <hr />
-
             <form className="">
                 <div className="flex flex-row justify-between w-full">
                     <div className="flex flex-col w-1/2">
