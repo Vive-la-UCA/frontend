8ef055f1e55ba5edab4062e00187a57722a2830e
Josse2002 <00048322@uca.edu.sv>
2024-03-25 17:41:45 -0600

    :safety_vest: Add validations to create new location

--- src/components/MapLocation.jsx
+++ src/components/MapLocation.jsx
@@ -2,7 +2,6 @@
 import 'leaflet/dist/leaflet.css';
 import { MapContainer, TileLayer, Marker, useMapEvents } from 'react-leaflet'
 import { icon } from 'leaflet';
-import { useState } from 'react';
 
 const ICON = icon({
     iconUrl: "https://images.ctfassets.net/3prze68gbwl1/assetglossary-17su9wok1ui0z7w/c4c4bdcdf0d0f86447d3efc450d1d081/map-marker.png",
