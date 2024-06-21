ee65987024cbccd0fa3f9b371e66f4ef2e7bfa6f
Bryan31542 <00063820@uca.edu.sv>
2024-06-20 22:19:44 -0600

    :sparkles: Feat: change API_URL

--- src/app/constants/session.js
+++ src/app/constants/session.js
@@ -1,5 +1,5 @@
-// export const CORE_API_URL = "https://vivelauca.uca.edu.sv/admin-back/api";
-// export const CORE_IMAGES_URL = "https://vivelauca.uca.edu.sv/admin-back";
+export const CORE_API_URL = 'https://vivelauca.uca.edu.sv/admin-back/api'
+export const CORE_IMAGES_URL = 'https://vivelauca.uca.edu.sv/admin-back'
 
-export const CORE_API_URL = 'http://localhost:5050/api'
-export const CORE_IMAGES_URL = 'http://localhost:5050'
+// export const CORE_API_URL = 'http://localhost:5050/api'
+// export const CORE_IMAGES_URL = 'http://localhost:5050'
